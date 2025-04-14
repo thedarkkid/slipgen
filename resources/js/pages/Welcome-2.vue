81d20d779dd52b39cb48693742f7711b43fac88a
Layo Folaranmi <thedarkkid.codes@gmail.com>
2025-04-14 11:30:08 +0100

    re-implemented with breeze

---
+++ resources/js/pages/Welcome-2.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Head, } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8">
+        Hello there
+    </div>
+</template>
